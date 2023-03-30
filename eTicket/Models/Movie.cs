aaebc3fe196b109af619472b97535a7a8209b143
SandeshKolhe <sandeshkolhe1098@gmail.com>
2023-03-30 17:14:26 +0530

    first  commit

---
+++ eTicket/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using eTicket.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationship
+        public List<Actor_Movie> Actor_Movies { get; set; }
+
+        //Cenema
+        public int CinemaID { get;  set; }
+        [ForeignKey("CinemaID")]
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+
+        public Producer Producer { get; set; }  
+
+
+    }
+}
